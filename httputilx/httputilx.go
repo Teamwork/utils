3a830eb5e73982f53a8bdb1be8b5ba44ff5e6ce2
Martin Tournoij <martin@arp242.net>
2017-10-20 09:00:22 +0100

    Also move modules/fetch to httputilx

--- httputilx/httputilx.go
+++ httputilx/httputilx.go
@@ -3,10 +3,17 @@ package httputilx // import "github.com/teamwork/utils/httputilx"
 
 import (
 	"bytes"
+	"fmt"
 	"io"
+	"io/ioutil"
 	"net/http"
 	"net/http/httputil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 
+	"github.com/pkg/errors"
 	"github.com/teamwork/utils/ioutilx"
 )
 
@@ -46,4 +53,87 @@ func DumpBody(r *http.Request) ([]byte, error) {
 
 	r.Body = save
 	return b.Bytes(), nil
+}
+
+// ErrNotOK is used when the status code is not 200 OK.
+type ErrNotOK struct {
+	URL string
+	Err string
+}
+
+func (e ErrNotOK) Error() string {
+	return fmt.Sprintf("code %v while downloading %v", e.Err, e.URL)
+}
+
+// Fetch the contents of an HTTP URL.
+//
+// This is not intended to cover all possible use cases  for fetching files,
+// only the most common ones. Use the net/http package for more advanced usage.
+func Fetch(url string) ([]byte, error) {
+	client := http.Client{Timeout: 60 * time.Second}
+	response, err := client.Get(url)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot download %v", url)
+	}
+	defer response.Body.Close() // nolint: errcheck
+
+	// TODO: Maybe add sanity check to bail out of the Content-Length is very
+	// large?
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return nil, errors.Wrapf(err, "cannot read body of %v", url)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return data, ErrNotOK{
+			URL: url,
+			Err: fmt.Sprintf("%v %v", response.StatusCode, response.Status),
+		}
+	}
+
+	return data, nil
+}
+
+// Save an HTTP URL to the directory dir with the filename. The filename can be
+// generated from the URL if empty.
+//
+// It will return the full path to the save file. Note that it may create both a
+// file *and* return an error (e.g. in cases of non-200 status codes).
+//
+// This is not intended to cover all possible use cases  for fetching files,
+// only the most common ones. Use the net/http package for more advanced usage.
+func Save(url string, dir string, filename string) (string, error) {
+	// Use last path of url if filename is empty
+	if filename == "" {
+		tokens := strings.Split(url, "/")
+		filename = tokens[len(tokens)-1]
+	}
+	path := filepath.FromSlash(dir + "/" + filename)
+
+	client := http.Client{Timeout: 60 * time.Second}
+	response, err := client.Get(url)
+	if err != nil {
+		return "", errors.Wrapf(err, "cannot download %v", url)
+	}
+	defer response.Body.Close() // nolint: errcheck
+
+	output, err := os.Create(path)
+	if err != nil {
+		return "", errors.Wrapf(err, "cannot create %v", path)
+	}
+	defer output.Close() // nolint: errcheck
+
+	_, err = io.Copy(output, response.Body)
+	if err != nil {
+		return path, errors.Wrapf(err, "cannot read body of %v in to %v", url, path)
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return path, ErrNotOK{
+			URL: url,
+			Err: fmt.Sprintf("%v %v", response.StatusCode, response.Status),
+		}
+	}
+
+	return path, nil
 }
--- httputilx/httputilx_test.go
+++ httputilx/httputilx_test.go
@@ -15,6 +15,8 @@ import (
 	"runtime"
 	"strings"
 	"testing"
+
+	"github.com/teamwork/test"
 )
 
 type dumpTest struct {
@@ -130,4 +132,34 @@ func mustReadRequest(s string) *http.Request {
 		panic(err)
 	}
 	return req
+}
+
+// TODO: better to not depend on interwebz...
+func TestFetch(t *testing.T) {
+	cases := []struct {
+		in, want, wantErr string
+	}{
+		{"http://example.com", "<html>", ""},
+		{"http://fairly-certain-this-doesnt-exist-asdasd12g1ghdfddd.com", "", "cannot download"},
+		{"http://httpbin.org/status/400", "", "400"},
+		{"http://httpbin.org/status/500", "", "500"},
+		// Make sure we return the body as well.
+		{"http://httpbin.org/status/418", "teapot", "418"},
+	}
+
+	for _, tc := range cases {
+		t.Run(fmt.Sprintf("%v", tc.in), func(t *testing.T) {
+			out, err := Fetch(tc.in)
+			if !test.ErrorContains(err, tc.wantErr) {
+				t.Errorf("wrong error\nout:  %#v\nwant: %#v\n", err, tc.wantErr)
+			}
+			if !strings.Contains(string(out), tc.want) {
+				t.Errorf("\nout:  %#v\nwant: %#v\n", string(out), tc.want)
+			}
+		})
+	}
+}
+
+// TODO: Add test.
+func TestSave(t *testing.T) {
 }
