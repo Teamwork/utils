dd27a977cfc7a790bf8cfedb0f25689ee39ed700
Martin Tournoij <martin@arp242.net>
2018-02-27 19:30:46 +0000

    Fix import

--- stringutil/stringutil.go
+++ stringutil/stringutil.go
@@ -1,5 +1,5 @@
 // Package stringutil adds functions for working with strings.
-package stringutil // import "github.com/teamwork/stringutil"
+package stringutil // import "github.com/teamwork/utils/stringutil"
 
 import "regexp"
 
