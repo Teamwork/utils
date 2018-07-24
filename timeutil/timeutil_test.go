71d381b3b15b440d8b4e6ab4c696c892021ec68d
Rafael Dantas Justo <rafael.justo@teamwork.com>
2018-07-24 14:16:13 +0100

    Some useful functions to retrieve the first and last day of the week

--- timeutil/timeutil_test.go
+++ timeutil/timeutil_test.go
@@ -46,6 +46,46 @@ func TestEndOfMonth(t *testing.T) {
 	}
 }
 
+func TestStartOfWeek(t *testing.T) {
+	cases := []struct {
+		in   time.Time
+		want time.Time
+	}{
+		{mustParse(t, "2016-01-13"), mustParse(t, "2016-01-11")},
+		{mustParse(t, "2016-01-01"), mustParse(t, "2015-12-28")},
+		{mustParse(t, "2016-12-30"), mustParse(t, "2016-12-26")},
+	}
+
+	for _, c := range cases {
+		got := StartOfWeek(c.in)
+		if got != c.want {
+			t.Errorf("StartOfWeek(%s) => %s, want %s", c.in, got, c.want)
+		}
+	}
+}
+
+func TestEndOfWeek(t *testing.T) {
+	cases := []struct {
+		in   time.Time
+		want time.Time
+	}{
+		{mustParse(t, "2016-01-01"), mustParse(t, "2016-01-03")},
+		{mustParse(t, "2016-01-31"), mustParse(t, "2016-01-31")},
+		{mustParse(t, "2016-11-01"), mustParse(t, "2016-11-06")},
+		{mustParse(t, "2016-12-31"), mustParse(t, "2017-01-01")},
+		// leap test
+		{mustParse(t, "2012-02-01"), mustParse(t, "2012-02-05")},
+		{mustParse(t, "2013-02-01"), mustParse(t, "2013-02-03")},
+	}
+
+	for _, c := range cases {
+		got := EndOfWeek(c.in)
+		if got != c.want {
+			t.Errorf("EndOfWeek(%s) => %s, want %s", c.in, got, c.want)
+		}
+	}
+}
+
 // mustParse parses value in the format YYYY-MM-DD failing the test on error.
 func mustParse(t *testing.T, value string) time.Time {
 	const layout = "2006-01-02"
