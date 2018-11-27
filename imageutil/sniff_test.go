865a901206a6777e55b370f1b7878a69e9630f71
Martin Tournoij <martin@arp242.net>
2018-11-27 19:40:44 +1300

    Add imageutil.DetectImageStream
    To detect image type from a file pointer.

--- imageutil/sniff_test.go
+++ imageutil/sniff_test.go
@@ -1,13 +1,16 @@
 package imageutil
 
 import (
+	"bytes"
+	"io/ioutil"
 	"net/http"
+	"reflect"
 	"testing"
 
 	"github.com/teamwork/test/image"
 )
 
-func TestDetectContentType(t *testing.T) {
+func TestDetectImage(t *testing.T) {
 	if ct := DetectImage(image.GIF); ct != "image/gif" {
 		t.Error(ct)
 	}
@@ -16,6 +19,25 @@ func TestDetectContentType(t *testing.T) {
 	}
 	if ct := DetectImage(image.PNG); ct != "image/png" {
 		t.Error(ct)
+	}
+}
+
+func TestDetectImageStream(t *testing.T) {
+	fp := bytes.NewReader(image.JPEG)
+	ct, err := DetectImageStream(fp)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if ct != "image/jpeg" {
+		t.Error(ct)
+	}
+
+	// No Tell() in Go? Hmm. Just compare data to see if Seek() worked.
+	d, _ := ioutil.ReadAll(fp)
+	if !reflect.DeepEqual(d, image.JPEG) {
+		t.Errorf("read data wrong; first 20 bytes:\nwant: %#v\ngot:  %#v",
+			image.JPEG[:19], d[:19])
 	}
 }
 
