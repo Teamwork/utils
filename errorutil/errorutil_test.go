9323aed3f3d9f0d914a2bd3a9fa44e219f4c0333
Martin Tournoij <martin@arp242.net>
2018-11-28 17:39:17 +1300

    Add errorutil.FilterTrace
    This is something I've been annoyed about for quite some time: the
    length of our stack traces. They're very long, and I constantly need to
    scroll my terminal to see the error and/or any context before it (as
    they're too large to fit on my window, even at full screen). It's pretty
    annoying.
    
    For example, returning an error from the `mysql` repo in Hub returns 32
    stack frames right now, of which only four are useful:
    
        github.com/teamwork/hub/mysql.(*InstallationRepository).Get
                /home/martin/work/src/github.com/teamwork/hub/mysql/installation.go:28
        github.com/teamwork/hub.(*InstallationService).Get
                /home/martin/work/src/github.com/teamwork/hub/installation.go:29
        github.com/teamwork/hub/api.Installations.get
                /home/martin/work/src/github.com/teamwork/hub/api/installations.go:58
        github.com/teamwork/hub/api.Installations.get-fm
                /home/martin/work/src/github.com/teamwork/hub/api/installations.go:35
    
    This is an issue both in our echo error handler and panic recoverer.
    
    With this function we can do some filtering; I already did an ad-hoc
    experiment in Hub here: https://github.com/Teamwork/hub/compare/trace
    and that seems to work well.
    
    I think that for most apps we can just filter everything except the app
    package; e.g. `re:github.com/teamwork/hub/.*`, instead of writing an
    extensive blacklist. Either way, this will allow us to do either.
    
    The biggest potential issue is what might happen if a panic occurs
    somewhere outside the app; I think that in most cases the entire trace
    should get filtered, meaning everything will get shown. If not, I can
    see about handling that in the `log` package.

---
+++ errorutil/errorutil_test.go
@@ -0,0 +1,67 @@
+package errorutil
+
+import (
+	"io/ioutil"
+	"testing"
+
+	"github.com/pkg/errors"
+)
+
+func TestFilterExclude(t *testing.T) {
+	err := makeErr()
+	err = errors.Wrap(err, "w00t")
+	err = errors.Wrap(err, "context")
+	err = FilterTrace(err, FilterPattern(FilterTraceExlude,
+		"testing",
+		"re:.*github.com/teamwork/utils/.*",
+	))
+
+	tErr, _ := err.(stackTracer)
+	if len(tErr.StackTrace()) != 1 {
+		t.Errorf("wrong length for stack trace: %d; wanted 1", len(tErr.StackTrace()))
+		for _, f := range tErr.StackTrace() {
+			t.Logf("%+v\n", f)
+		}
+	}
+}
+
+func TestFilterExcludeAll(t *testing.T) {
+	err := makeErr()
+	err = errors.Wrap(err, "w00t")
+	err = errors.Wrap(err, "context")
+	err = FilterTrace(err, FilterPattern(FilterTraceExlude, "re:.*"))
+
+	tErr, _ := err.(stackTracer)
+	if len(tErr.StackTrace()) != 3 {
+		t.Errorf("wrong length for stack trace: %d; wanted 3", len(tErr.StackTrace()))
+		for _, f := range tErr.StackTrace() {
+			t.Logf("%+v\n", f)
+		}
+	}
+}
+
+func TestFilterInclude(t *testing.T) {
+	err := makeErr()
+	err = errors.Wrap(err, "w00t")
+	err = errors.Wrap(err, "context")
+	err = FilterTrace(err, FilterPattern(FilterTraceInclude,
+		"re:.*github.com/teamwork/utils/.*"))
+
+	tErr, _ := err.(stackTracer)
+	if len(tErr.StackTrace()) != 1 {
+		t.Errorf("wrong length for stack trace: %d; wanted 1", len(tErr.StackTrace()))
+		for _, f := range tErr.StackTrace() {
+			t.Logf("%+v\n", f)
+		}
+	}
+}
+
+func makeErr() error {
+	err := zxc()
+	return errors.WithStack(err)
+}
+
+func zxc() error {
+	_, err := ioutil.ReadFile("/var/empty/nonexistent")
+	return errors.Wrap(err, "could not read")
+}
